a0793a11097bce6884bfc8a345fd8e19260f8a74
D3Portillo <d3portillo@gmail.com>
2022-11-14 00:43:15 +0100

    chores: add skeleton layout

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/*.{tsx,js}",
+    "./pages/**/*.{tsx,js}",
+    "./components/*.{tsx,js}",
+    "./components/**/*.{tsx,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
